682a59f494cb3bb78bacc1002bb117517c627b49
miro-tech <62233256+miro-tech@users.noreply.github.com>
2026-07-31 12:49:22 +0500

    Update update_gist.py

--- update_gist.py
+++ update_gist.py
@@ -136,7 +136,7 @@ def get_vless_links(target):
                 if not proxy: continue
 
                 vnext = proxy["settings"]["vnext"][0]
-                vnext["address"] = "8.47.69.0"
+                vnext["address"] = "8.6.112.0"
                 user = vnext["users"][0]
                 stream = proxy["streamSettings"]
                 
